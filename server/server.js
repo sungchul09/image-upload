e91a7f0110f5ee2beaf1da1ba8831408be55a317
seongcheolyun <seongcheol.yun@flitto.com>
2022-09-04 00:32:41 +0900

    feat: server-init

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const multer = require('multer')
+const { v4: uuid } = require('uuid')
+const mime = require('mime-types')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "./uploads"),
+  filename: (req, file, cb) => cb(null, `${uuid()}.${mime.extension(file.mimetype)}`)
+})
+
+const upload = multer({ storage, 
+  fileFilter: (req, file, cb) => {
+    if(['image/jpeg', 'image/png'].includes(file.mimetype)) cb(null, true)
+    else cb(new Error('invalid file type'), false)
+  },
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  } 
+})
+
+const app = express()
+const PORT = 5001
+
+app.use("/uploads", express.static('uploads'))
+
+app.post('/upload', upload.single('imageTest'), (req, res) => {
+  console.log(req.file)
+  res.json(req.file)
+})
+
+app.listen(PORT, 
+  () => console.log('expree server listening on PORT ' + PORT)
+)
